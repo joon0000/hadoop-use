652dac659a4172f27fd4486efc36962ccd3a099d
joon0000 <junekawin@icloud.com>
2026-01-19 20:32:09 +0700

    del hadoop-yarn-project,hadoop-mapreduce-project

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-capacity-scheduler-ui/src/main/webapp/react-router.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
-  appDirectory: "src/app",
-  basename: "/scheduler-ui",
-} satisfies Config;
